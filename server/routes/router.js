8f8643fe4647ef53293485efa15124d2f260c58d
vaishnavi-kotturu <58888075+vaishnavi-kotturu@users.noreply.github.com>
2021-04-23 23:30:54 +0530

    Add files via upload

---
+++ server/routes/router.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const route=express.Router();
+
+const services=require('../services/render');
+const controller=require('../controller/controller');
+// @description Root Route, @method GET
+
+route.get('/', services.homeRoutes)
+
+// @description Add Users Route, @method GET
+
+route.get('/add-user', services.add_user)
+
+// @description Update Users Route, @method GET
+
+route.get('/update-user',services.update_user)
+
+//API
+route.post('/api/users',controller.create);
+route.get('/api/users',controller.find);
+route.put('/api/users/:id',controller.update);
+route.delete('/api/users/:id',controller.delete);
+
+
+module.exports=route
