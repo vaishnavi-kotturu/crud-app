b7e0ea91ac1a8bb8000e39d25bd97b46b664c6c1
Kumar <utkarshshanu@gmail.com>
2021-05-01 12:50:39 +0530

    Location made non mandatory

--- server/routes/router.js
+++ server/routes/router.js
@@ -7,68 +7,21 @@ const axios=require('axios');
 
 const services=require('../services/render');
 const controller=require('../controller/controller');
-// @description Root Route, @method GET
 
+// @description Root Route, @method GET
 route.get('/', services.homeRoutes)
 
 // @description Add Users Route, @method GET
+route.get('/add-user', services.add_user);
 
-
-// route.get('/helpline', services.helpline)
-route.get('/helpline',(req,res)=>{
-    let one = `http://localhost:${PORT}/api/helpline`;
-    let two = `http://localhost:${PORT}/api/city`;
-    const requestOne = axios.get(one);
-    const requestTwo = axios.get(two);
-
-    axios.all([requestOne, requestTwo]).then(axios.spread((...responses) => {
-        const responseOne = responses[0];
-        const responseTwo = responses[1];
-        const cities = responseTwo.data; 
-        res.render('helpline',{helpline:responseOne.data, cities}); 
-      })).catch(err => { 
-            res.send(err);
-      })
-
-        // .then(function(numberdata){
-        //     const number = numberdata.data; 
-        //     res.render("helpline",{number})
-        // })
-        // .catch(err=>{
-        //     res.send(err);
-        // })
-})
+// @description Route for viewing helpline, @method GET
+route.get('/helpline', services.helpline)
 
 // @description Update Users Route, @method GET
-
 route.get('/update-user', services.update_user)
 
-route.get('/filter-city',(req,res)=>{
-    let one = `http://localhost:${PORT}/api/filter`;
-    let two = `http://localhost:${PORT}/api/city`;
-    const requestOne = axios.get(one,{params:{city:req.query.city}});
-    const requestTwo = axios.get(two);
+route.get('/filter-city', services.filter_city)
 
-    axios.all([requestOne, requestTwo]).then(axios.spread((...responses) => {
-        const responseOne = responses[0];
-        const responseTwo = responses[1];
-        const cities = responseTwo.data; 
-        res.render('index',{users:responseOne.data, cities}); 
-      })).catch(err => { 
-            res.send(err);
-      })
-})
-
-route.get('/add-user',(req,res)=>{
-    axios.get(`http://localhost:${PORT}/api/city`)
-        .then(function(citydata){
-            const cities = citydata.data; 
-            res.render("add_user",{cities})
-        })
-        .catch(err=>{
-            res.send(err);
-        })
-})
 
 //API
 route.post('/api/users',controller.create);
--- server/services/render.js
+++ server/services/render.js
@@ -22,15 +22,33 @@ exports.homeRoutes=(req,res)=>{
 }
 
 exports.add_user=(req,res)=>{
-    res.render('add_user')
+    axios.get(`http://localhost:${PORT}/api/city`)
+        .then(function(citydata){
+            const cities = citydata.data; 
+            res.render("add_user",{cities})
+        })
+        .catch(err=>{
+            res.send(err);
+        })
 }
 
 exports.helpline=(req,res)=>{
-    res.render('helpline')
+    let one = `http://localhost:${PORT}/api/helpline`;
+    let two = `http://localhost:${PORT}/api/city`;
+    const requestOne = axios.get(one);
+    const requestTwo = axios.get(two);
+
+    axios.all([requestOne, requestTwo]).then(axios.spread((...responses) => {
+        const responseOne = responses[0];
+        const responseTwo = responses[1];
+        const cities = responseTwo.data; 
+        res.render('helpline',{helpline:responseOne.data, cities}); 
+      })).catch(err => { 
+            res.send(err);
+      })
 }
 
 exports.update_user=(req,res)=>{
-
     let one = `http://localhost:${PORT}/api/users`;
     let two = `http://localhost:${PORT}/api/city`;
     const requestOne = axios.get(one,{params:{id:req.query.id}});
@@ -51,4 +69,20 @@ exports.update_user=(req,res)=>{
     //     .catch(err=>{
     //         res.send(err);
     //     })
+}
+
+exports.filter_city=(req,res)=>{
+    let one = `http://localhost:${PORT}/api/filter`;
+    let two = `http://localhost:${PORT}/api/city`;
+    const requestOne = axios.get(one,{params:{city:req.query.city}});
+    const requestTwo = axios.get(two);
+
+    axios.all([requestOne, requestTwo]).then(axios.spread((...responses) => {
+        const responseOne = responses[0];
+        const responseTwo = responses[1];
+        const cities = responseTwo.data; 
+        res.render('index',{users:responseOne.data, cities}); 
+      })).catch(err => { 
+            res.send(err);
+      })
 }
